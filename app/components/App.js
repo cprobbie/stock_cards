f9e9f79c8f447be3657f6b27c5206cb6737810fc
Robbie <cprobbie23@gmail.com>
2018-02-28 19:42:17 +1100

    added bigCard Chart UI almost done

--- app/components/App.js
+++ app/components/App.js
@@ -4,8 +4,37 @@ import RaisedButton from 'material-ui/RaisedButton';
 import LeaderBoard from './LeaderBoard'
 import Main from "./Main"
 import Drawer from 'material-ui/Drawer';
-import BigCard from './BigCard'
-import MenuItem from 'material-ui/MenuItem';
+import BigCard from './BigCard';
+import LineChart from 'react-chartkick';
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Stock Price',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
 
 export default class App extends React.Component {
 
@@ -50,11 +79,13 @@ export default class App extends React.Component {
       <Drawer
         ref="myRef"
         docked={false}
-        width={500}
+        width={570}
         open={this.state.open}
         onRequestChange={(open) => this.setState({ open })}
       >
         <BigCard />
+        <p style={{textAlign:'center'}} >* $20 brokage fee is charged on each transaction</p>
+        <Line data={data} />
       </Drawer>
       <footer style={{backgroundColor:'yellow'}} >2018</footer>
     </div>
--- app/components/BigCard.js
+++ app/components/BigCard.js
@@ -1,34 +1,43 @@
 import React from 'react'
 import './BigCard.scss'
-import { List, ListItem } from 'material-ui/List';
+import { RaisedButton } from 'material-ui';
 
-const listStyle = {
-  height: 100,
-  width: 100,
-  display: 'inline-block',
-  fontSize: 30
-}
 
 export default class BigCard extends React.Component {
 
   render() {
     return (
-      <div className="card">
-        <header>Code: CBA</header>
+      <div className="bigCard">
+        <header style={{fontSize:18}}>Code: CBA</header>
         <h4>Commonweath Bank Australia</h4>
         <div className='content'>
-          <span className='price' >80.08</span>
-          <div style={{ display: 'flex', justifyContent: 'space-around' }} >
+          <div style={{ display: 'flex',justifyContent:'space-around' }} >
+            <span className='price' >80.08</span>
             <span className='change' >+1.2</span>
             <span className='pChange' >+1%</span>
           </div>
           <div style={{ display: 'flex', justifyContent: 'space-around' }} >
-            <List style={listStyle}>
-              <ListItem primaryText="50" secondaryText="Holding" />
-            </List>
-            <List style={listStyle}>
-              <ListItem primaryText="40000" secondaryText="Value" />
-            </List>
+            <ul>
+              <li>Previous Close: 75.46</li>
+              <li>52 Week High: 87.74</li>
+              <li>52 Week Low: 73.2</li>
+              <li>P/E Ratio: 13.36</li>
+              <li>Market Cap: 132B</li>
+            </ul>
+            <ul>
+              <li>Sector: Bank</li>
+              <li>EPS: 556.4</li>
+              <li>DPS: 429.0</li>
+              <li>Dividend: 5.7%</li>
+            </ul>
+          </div>
+          <div style={{display:'flex', justifyContent:'space-around', margin:'10px 10px'}} >
+            <input style={{height:40,fontSize:18}} type='number' />
+            <span style={{fontSize:30}} >Value: 760.7</span>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <RaisedButton primary={true} label='BUY' style={{width:200}} />
+            <RaisedButton secondary={true} label='SELL' style={{ width: 200 }}/>
           </div>
         </div>
       </div>
